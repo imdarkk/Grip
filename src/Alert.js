4110e46fb686558f72114b503c2eb16606040958
DarkDevCY <60070568+DarkDevCY@users.noreply.github.com>
2021-02-05 19:38:57 +0200

    Created project, added generation and disclaimer

---
+++ src/Alert.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './styles/Alert.scss';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle, faTimes } from "@fortawesome/fontawesome-free-solid";
+
+class Alert extends React.Component {
+    render() {
+        return (
+          <div className="wrapperAlert">
+            <FontAwesomeIcon icon={faExclamationTriangle} id="warningSign" />
+            <div id="wrapperText">
+              <p className="textAlert">
+                We have no responsibility if your password gets leaked or
+                hacked.
+              </p>
+              <p className="textAlert">
+                We suggest saving the password after using it, or using the
+                google password manager.
+              </p>
+                </div>
+                <FontAwesomeIcon icon={faTimes} id="closeBtn" onClick={this.props.hide}/>
+          </div>
+        );
+    }
+}
+
+export default Alert;
---
+++ src/App.js
@@ -0,0 +1,91 @@
+import "./styles/App.scss";
+import "./styles/slid.css";
+
+import React from "react";
+import Alert from "./Alert";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "8", isHidden: true };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    var result = "";
+
+    function getRandomString(length) {
+      var randomChars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*/><.,?)_(+=-`~";
+      for (var i = 0; i < length; i++) {
+        result += randomChars.charAt(
+          Math.floor(Math.random() * randomChars.length)
+        );
+      }
+      return result;
+    }
+
+    getRandomString(this.state.value);
+    return (
+      <div className="App">
+        <div id="mainWrapper">
+          <input
+            type="range"
+            className="slider"
+            min={0}
+            max={55}
+            step={1}
+            defaultValue={8}
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <p id="amountOfChars">
+            Length: <span className="pass">{this.state.value}</span>
+          </p>
+          <div id="wrapper">
+            {result.length > 7 ? (
+              <div id="wrapperResult">
+                <span id="resultText">
+                  Password: <br />
+                  <span className="pass password">{result}</span>
+                </span>
+              </div>
+            ) : (
+              <div>
+                <p id="disclaimer">
+                  <span id="discBold">Disclaimer:</span> We recommend a password
+                  more than 8 characters in length. <br />
+                </p>
+                <div id="wrapperResult">
+                  <span id="resultText">
+                    Password: <br />
+                    <span className="pass password">{result}</span>
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div id="wrapperDisclaimerBtn">
+          <p
+            id="disclaimerButton"
+            onClick={() => {
+              this.setState({ isHidden: false });
+            }}
+          >
+            Disclaimer
+          </p>
+        </div>
+        {!this.state.isHidden && (
+          <Alert hide={() => this.setState({ isHidden: true })} />
+        )}
+      </div>
+    );
+  }
+}
+export default App;
